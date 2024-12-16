cf55f5c0ea1b1c5e2db67084b7bce9ce8270602e
Eduardo <etorres@auvik.com>
2024-12-16 16:22:43 -0500

    base code

---
+++ src/app/components/SearchBox.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: aquamarine;
+`
+
+
+export const SearchBar = () => {
+  console.log();
+
+  return <div className="text-3xl font-bold underline"><StyledDiv>SearchBarX</StyledDiv></div>
+}
---
+++ src/app/recipe/[slug]/page.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+const Recipe = () => <div>super cool recipe</div>
+
+export default Recipe;
